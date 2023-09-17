840b5d6768a03145426e5505c1b6803a43b5c36b
ritalisboar <cassialisboar@gmail.com>
2023-09-17 19:39:50 -0300

    inclusão dos recursos IA

--- server/index.js
+++ server/index.js
@@ -1,13 +1,36 @@
 import cors from 'cors'
-import express from 'express'
+import express, { response } from 'express'
+
+import { convert } from "./convert.js"
 import { download } from './download.js'
+import { transcribe } from './transcribe.js'
+import { summarize } from './summarize.js'
+import { request } from 'http'
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get('/summary/:id', (request, response) => {
-  download(request.params.id)
-  response.json({ result: "Download realizado com sucesso" })
+app.get('/summary/:id', async (request, response) => {
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    const result = await transcribe(audioConverted) 
+    return response.json({ result: result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
+app.post('/summary', async (request, response) => {
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result: result })
+  } catch (error) {
+    console.log(error)
+    return response.json({ error })
+  }
+
+})
 app.listen(3333, () => console.log("Server is running on port 3333"))
